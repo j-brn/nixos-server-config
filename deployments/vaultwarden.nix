65738f271e63c3a764a603e6211e50f288db1ba9
j-brn <me@bricker.io>
2022-10-03 14:04:25 +0200

    Configure mailer for vaultwarden

--- deployments/vaultwarden.nix
+++ deployments/vaultwarden.nix
@@ -11,10 +11,27 @@
           "VIRTUAL_HOST" = host;
           "LETSENCRYPT_HOST" = host;
           "VIRTUAL_PORT" = 80;
+          "DOMAIN" = "https://${host}";
+
+          # TODO: find a better mailing solution
+          "SMTP_HOST" = "mailer";
+          "SMTP_FROM" = "noreply@vaultwarden.bricker.io";
+          "SMTP_PORT" = 25;
+          # OK because this mailer is only reachable from within the "mailer" network
+          "SMTP_SSL" = "false";
         };
         env_file = [ environmentFilePath ];
         volumes = [ "vaultwarden_data:/data" ];
-        networks = [ "proxy-network" ];
+        networks = [
+          "proxy-network"
+          "mailer"
+        ];
+      };
+
+      mailer.service = {
+        image = "ixdotai/smtp:latest";
+        restart = "unless-stopped";
+        networks = [ "mailer" ];
       };
     };
 
@@ -22,7 +39,9 @@
       proxy-network = {
         external = true;
       };
+      mailer = {};
     };
+
     docker-compose.raw = {
       volumes = {
         vaultwarden_data = { };
