93c38cb1b6fe83084ae0396255a16a0ce14668a4
j-brn <me@bricker.io>
2022-10-15 02:57:35 +0200

    Flake update

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@ in
 {
   inherit imports;
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
