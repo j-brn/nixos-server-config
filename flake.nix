3a414c8dac5c0c7911b396d4c1bf776b7e0bbfc5
j-brn <me@jbrn.eu>
2022-12-11 15:39:35 +0100

    remove hosts.txt

--- flake.nix
+++ flake.nix
@@ -2,103 +2,115 @@
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
     deploy-rs = { url = "github:serokell/deploy-rs"; inputs.nixpkgs.follows = "nixpkgs"; };
-    utils = { url = "github:numtide/flake-utils"; };
+    flake-parts.url = "github:hercules-ci/flake-parts";
     agenix = { url = "github:ryantm/agenix"; inputs.nixpkgs.follows = "nixpkgs"; };
     arion = { url = "github:hercules-ci/arion"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = { self, nixpkgs, deploy-rs, utils, agenix, arion, ... }@inputs:
-    {
-      nixosConfigurations = {
-        "kashyyyk" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs self; };
-          modules = [
-            agenix.nixosModule
-            arion.nixosModules.arion
-            ./default.nix
-            ./hosts/kashyyyk/configuration.nix
-          ];
-        };
+  outputs = { self, nixpkgs, deploy-rs, flake-parts, agenix, arion, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit self; } {
+      flake = {
+        nixosConfigurations = {
+          "kashyyyk" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs self; };
+            modules = [
+              agenix.nixosModule
+              arion.nixosModules.arion
+              ./default.nix
+              ./hosts/kashyyyk/configuration.nix
+            ];
+          };
 
-        "bespin" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs self; };
-          modules = [
-            agenix.nixosModule
-            arion.nixosModules.arion
-            ./default.nix
-            ./hosts/bespin/configuration.nix
-          ];
-        };
+          "bespin" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs self; };
+            modules = [
+              agenix.nixosModule
+              arion.nixosModules.arion
+              ./default.nix
+              ./hosts/bespin/configuration.nix
+            ];
+          };
 
-        "scarif" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs self; };
-          modules = [
-            agenix.nixosModule
-            arion.nixosModules.arion
-            ./default.nix
-            ./hosts/scarif/configuration.nix
-          ];
-        };
-      };
-
-      deploy.nodes = {
-        kashyyyk = {
-          sshOpts = [ "-p" "222" ];
-          hostname = "kashyyyk.bricker.io";
-          fastConnection = true;
-
-          profiles.system = {
-            sshUser = "admin";
-            path =
-              deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.kashyyyk;
-            user = "root";
+          "scarif" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs self; };
+            modules = [
+              agenix.nixosModule
+              arion.nixosModules.arion
+              ./default.nix
+              ./hosts/scarif/configuration.nix
+            ];
           };
         };
 
-        bespin = {
-          sshOpts = [ "-p" "222" ];
-          hostname = "bespin.bricker.io";
-          fastConnection = true;
+        deploy.nodes = {
+          kashyyyk = {
+            sshOpts = [ "-p" "222" ];
+            hostname = "kashyyyk.bricker.io";
+            fastConnection = true;
 
-          profiles.system = {
-            sshUser = "admin";
-            path =
-              deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.bespin;
-            user = "root";
+            profiles.system = {
+              sshUser = "admin";
+              path =
+                deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.kashyyyk;
+              user = "root";
+            };
+          };
+
+          bespin = {
+            sshOpts = [ "-p" "222" ];
+            hostname = "bespin.bricker.io";
+            fastConnection = true;
+
+            profiles.system = {
+              sshUser = "admin";
+              path =
+                deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.bespin;
+              user = "root";
+            };
+          };
+
+          scarif = {
+            sshOpts = [ "-p" "222" ];
+            hostname = "scarif.bricker.io";
+            fastConnection = true;
+
+            profiles.system = {
+              sshUser = "admin";
+              path =
+                deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.scarif;
+              user = "root";
+            };
           };
         };
+    };
 
-        scarif = {
-          sshOpts = [ "-p" "222" ];
-          hostname = "scarif.bricker.io";
-          fastConnection = true;
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-          profiles.system = {
-            sshUser = "admin";
-            path =
-              deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.scarif;
-            user = "root";
-          };
-        };
-      };
+    perSystem = { pkgs, lib, system, config, ... }: {
 
-      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-    } // utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+      # shell script to get the hostkeys for all deployment hosts
+      packages.keyscan = with lib; let
+        commands = mapAttrsToList
+          (_: node:
+            let optStr = concatStringsSep " " ([ "-H" "-t ed25519" ] ++ node.sshOpts);
+            in "ssh-keyscan ${optStr} ${node.hostname}")
+          self.deploy.nodes;
       in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            agenix.defaultPackage.${system}
-            deploy-rs.defaultPackage.${system}
-            apacheHttpd
-            nixpkgs-fmt
-          ];
-        };
-      }
-    );
+      pkgs.writeShellScriptBin "keyscan" (concatStringsSep "\n" commands);
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          config.packages.keyscan
+          agenix.defaultPackage.${system}
+          pkgs.deploy-rs
+          nixpkgs-fmt
+        ];
+      };
+
+      checks = deploy-rs.lib.${system}.deployChecks self.deploy;
+    };
+  };
 }
